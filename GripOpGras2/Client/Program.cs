08572c4ef0fcf904fc9a7c37b1c2062a96edb4cf
Chris <54854679+cvlnomen@users.noreply.github.com>
2022-12-05 11:57:23 +0100

    Cleanup client app

--- GripOpGras2/Client/Program.cs
+++ GripOpGras2/Client/Program.cs
@@ -3,17 +3,17 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace GripOpGras2.Client
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
+	public class Program
+	{
+		public static async Task Main(string[] args)
+		{
+			WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.RootComponents.Add<App>("#app");
+			builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            await builder.Build().RunAsync();
-        }
-    }
+			await builder.Build().RunAsync();
+		}
+	}
 }
--- GripOpGras2/Server/Program.cs
+++ GripOpGras2/Server/Program.cs
@@ -1,43 +1,41 @@
 namespace GripOpGras2.Server
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
+			builder.Services.AddRazorPages();
 
-            builder.Services.AddControllersWithViews();
-            builder.Services.AddRazorPages();
+			WebApplication app = builder.Build();
 
-            var app = builder.Build();
+			// Configure the HTTP request pipeline.
+			if (app.Environment.IsDevelopment())
+			{
+				app.UseWebAssemblyDebugging();
+			}
+			else
+			{
+				app.UseExceptionHandler("/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseWebAssemblyDebugging();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+			app.UseHttpsRedirection();
 
-            app.UseHttpsRedirection();
+			app.UseBlazorFrameworkFiles();
+			app.UseStaticFiles();
 
-            app.UseBlazorFrameworkFiles();
-            app.UseStaticFiles();
+			app.UseRouting();
 
-            app.UseRouting();
+			app.MapRazorPages();
+			app.MapControllers();
+			app.MapFallbackToFile("index.html");
 
-
-            app.MapRazorPages();
-            app.MapControllers();
-            app.MapFallbackToFile("index.html");
-
-            app.Run();
-        }
-    }
+			app.Run();
+		}
+	}
 }
