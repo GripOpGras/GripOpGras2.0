8342325180baa1b89ec8e7abb5a5abdbe50e426d
Chris <54854679+cvlnomen@users.noreply.github.com>
2022-12-05 14:16:18 +0100

    Added comments

--- GripOpGras2.Domain/Plot.cs
+++ GripOpGras2.Domain/Plot.cs
@@ -3,17 +3,17 @@
 	public class Plot
 	{
 		public string? Name { get; set; }
-		/// <summary>
-		/// In hectares
-		/// </summary>
-		public float Area { get; set; }
 		public DateTime LastMeasured { get; set; }
 		/// <summary>
-		/// In kg DM/ha
+		/// In hectares.
+		/// </summary>
+		public float Area { get; set; }
+		/// <summary>
+		/// In kg DM/ha.
 		/// </summary>
 		public float NetDryMatter { get; set; }
 		/// <summary>
-		/// In kg DM/ha
+		/// In kg DM/ha.
 		/// </summary>
 		public float DryMatterGrowth { get; set; }
 		public FeedAnalysis? FeedAnalysis { get; set; }
--- GripOpGras2.Domain/Ration.cs
+++ GripOpGras2.Domain/Ration.cs
@@ -1,12 +1,13 @@
-﻿using GripOpGras2.Domain;
-
-namespace GripOpGras2.Models
+﻿namespace GripOpGras2.Domain
 {
 	public class FeedRation
 	{
 		public Herd? Herd { get; set; }
 		public DateTime Date { get; set; }
 		public Plot? Plot { get; set; }
+		/// <summary>
+		/// The value is in kg roughage.
+		/// </summary>
 		public Dictionary<Roughage, float>? Roughages { get; set; }
 	}
 }
