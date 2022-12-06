e43f30ce9b9d1afde54b07673f869bf78ee93a38
Chris <54854679+cvlnomen@users.noreply.github.com>
2022-12-06 11:33:58 +0100

    Added new models

---
+++ GripOpGras2.Domain/Grazing.cs
@@ -0,0 +1,9 @@
+﻿namespace GripOpGras2.Domain
+{
+	public class Grazing : GrasslandCalendarActivity
+	{
+		public Herd? Herd { get; set; }
+
+		public Plot? Plot { get; set; }
+	}
+}
---
+++ GripOpGras2.Domain/MilkProductionAnalysis.cs
@@ -0,0 +1,12 @@
+﻿namespace GripOpGras2.Domain
+{
+	public class MilkProductionAnalysis
+	{
+		public DateTime Date { get; set; }
+
+		/// <summary>
+		/// The total amount of milk produced in liters.
+		/// </summary>
+		public float Amount { get; set; }
+	}
+}
