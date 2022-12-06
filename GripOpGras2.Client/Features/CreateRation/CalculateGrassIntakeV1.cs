869d1ae1854e174b4dae013fb8da679e71b8da37
Chris <54854679+cvlnomen@users.noreply.github.com>
2022-12-06 15:53:29 +0100

    Added first specflow unit test for grass intake

--- GripOpGras2.Client/Features/CreateRation/CalculateGrassIntakeV1.cs
+++ GripOpGras2.Client/Features/CreateRation/CalculateGrassIntakeV1.cs
@@ -5,10 +5,12 @@ namespace GripOpGras2.Client.Features.CreateRation
 	//TODO the implementation of this class needs to be moved to a separate module.
 	public class CalculateGrassIntakeV1 : ICalculateGrassIntake
 	{
+		private const float GrazingLosses = 0.15f;
+
 		public async Task<float> CalculateGrassIntakeAsync(Grazing grazingActivity)
 		{
 			float totalDryMatter = grazingActivity.Plot.NetDryMatter * grazingActivity.Plot.Area;
-			totalDryMatter = (float)(totalDryMatter - totalDryMatter * 0.15);
+			totalDryMatter -= totalDryMatter * GrazingLosses;
 
 			return await Task.FromResult(totalDryMatter);
 		}
---
+++ GripOpGras2.Specs/Features/CalculateGrassIntakeV1.feature
@@ -0,0 +1,13 @@
+﻿Feature: GripOpGras2.CalculateGrassIntakeV1
+
+The first approach devised to calculate the total grass intake of a herd
+
+Scenario: Calculate grass intake of a herd
+	Given my plot has a size of <plotArea> ha
+	And my plot has <netDryMatter> net kg dm/ha
+	When I calculate the total grass intake
+	Then the total grass intake of the herd should be <totalGrassIntake> kg dm
+Examples:
+| plotArea | netDryMatter | totalGrassIntake |
+| 10 | 2000 | 17000 |
+| 10 | 3000 | 25500 |
