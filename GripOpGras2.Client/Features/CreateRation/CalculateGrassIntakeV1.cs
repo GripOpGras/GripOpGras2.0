9659e9d5dc072eb4ee1e4426da1b1b8607643c25
Chris <54854679+cvlnomen@users.noreply.github.com>
2022-12-06 11:36:07 +0100

    Added grass intake and vem needs calculations

---
+++ GripOpGras2.Client/Features/CreateRation/CalculateGrassIntakeV1.cs
@@ -0,0 +1,16 @@
+﻿using GripOpGras2.Domain;
+
+namespace GripOpGras2.Client.Features.CreateRation
+{
+	//TODO the implementation of this class needs to be moved to a separate module.
+	public class CalculateGrassIntakeV1 : ICalculateGrassIntake
+	{
+		public async Task<float> CalculateGrassIntakeAsync(Grazing grazingActivity)
+		{
+			float totalDryMatter = grazingActivity.Plot.NetDryMatter * grazingActivity.Plot.Area;
+			totalDryMatter = (float)(totalDryMatter - totalDryMatter * 0.15);
+
+			return await Task.FromResult(totalDryMatter);
+		}
+	}
+}
---
+++ GripOpGras2.Client/Features/CreateRation/RationAlgorithmV1.cs
@@ -0,0 +1,33 @@
+﻿using GripOpGras2.Domain;
+
+namespace GripOpGras2.Client.Features.CreateRation
+{
+	public class RationAlgorithmV1 : IRationAlgorithm
+	{
+		/// <summary>
+		/// The standerd VEM needs of a cow
+		/// </summary>
+		private const float DefaultVEMNeeds = 5500;
+
+		private const float VemNeedsPerLiterMilk = 450;
+
+		private const float OptimalVEMCoverage = 1.05f;
+
+		public async Task<FeedRation> CreateRationAsync(IReadOnlyList<Roughage> roughages, Herd herd,
+			float totalGrassIntake, Grazing grazingActivity, MilkProductionAnalysis milkProductionAnalysis)
+		{
+			float vemNeeds = CalculateVemNeedsOfTheHerd(herd, milkProductionAnalysis);
+
+
+			throw new NotImplementedException();
+		}
+
+		public float CalculateVemNeedsOfTheHerd(Herd herd, MilkProductionAnalysis milkProductionAnalysis)
+		{
+			float milkProductionForEachCow = milkProductionAnalysis.Amount / herd.NumberOfAnimals;
+
+			return herd.NumberOfAnimals *
+				   ((milkProductionForEachCow * VemNeedsPerLiterMilk + DefaultVEMNeeds) * OptimalVEMCoverage);
+		}
+	}
+}
