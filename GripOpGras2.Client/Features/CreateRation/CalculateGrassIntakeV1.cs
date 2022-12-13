278d862a2a2bbb2d13460ab9006729389262010f
Chris <54854679+cvlnomen@users.noreply.github.com>
2022-12-13 17:16:39 +0100

    Fixed calculategrassintakev1

--- GripOpGras2.Client/Features/CreateRation/CalculateGrassIntakeV1.cs
+++ GripOpGras2.Client/Features/CreateRation/CalculateGrassIntakeV1.cs
@@ -5,12 +5,12 @@ namespace GripOpGras2.Client.Features.CreateRation
 	//TODO the implementation of this class needs to be moved to a separate module.
 	public class CalculateGrassIntakeV1 : ICalculateGrassIntake
 	{
-		private const float GrazingLosses = 0.15f;
+		private const float GrazingLosses = 0.85f;
 
 		public async Task<float> CalculateGrassIntakeAsync(GrazingActivity grazingActivity)
 		{
 			float totalDryMatter = grazingActivity.Plot.NetDryMatter * grazingActivity.Plot.Area;
-			totalDryMatter -= totalDryMatter * GrazingLosses;
+			totalDryMatter *= GrazingLosses;
 
 			return await Task.FromResult(totalDryMatter);
 		}
