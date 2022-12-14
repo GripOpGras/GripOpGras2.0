fcdf59306cfb23cdaacb229cd4215bca5950081e
Chris <54854679+cvlnomen@users.noreply.github.com>
2022-12-14 16:42:34 +0100

    Pussed half of the code

--- GripOpGras2.Client/Features/CreateRation/RationAlgorithmV1.cs
+++ GripOpGras2.Client/Features/CreateRation/RationAlgorithmV1.cs
@@ -5,26 +5,36 @@ namespace GripOpGras2.Client.Features.CreateRation
 {
 	public class RationAlgorithmV1 : IRationAlgorithm
 	{
-		/// <summary>
-		/// The standerd VEM needs of a cow
-		/// </summary>
-		private const float DefaultVEMNeeds = 5500;
+		private const float DefaultVEMNeedsOfCow = 5500;
 
 		private const float VemNeedsPerLiterMilk = 450;
 
+		private const float MaxAmountOfSupplementaryFeedProductInKGPerCow = 4.5f;
+
+		private const float MaxKgDmIntakePerCow = 18;
+
 		private const float OptimalVEMCoverage = 1.05f;
 
-		private const float OptimalRECoverage = 0.15f;
+		private const float OptimalRECoverageInGramsPerKgDm = 150;
 
-		private const float MaxRECoverage = 0.17f;
+		// TODO alleen deze gebruiken wanneer bij de uitkomsten is gebleken dat 150 niet te doen is, dus dat er opnieuw moet worden gewerkt
+		private const float MaxRECoverageInGramsPerKgDm = 170;
+
+		// TODO alleen deze gebruiken wanneer bij de uitkomsten is gebleken dat 150 niet te doen is, dus dat er opnieuw moet worden gewerkt
+		private const float MinRECoverageInGramsPerKgDm = 140;
 
 		public async Task<FeedRation> CreateRationAsync(IReadOnlyList<FeedProduct> feedProducts, Herd herd,
 			float totalGrassIntake, MilkProductionAnalysis milkProductionAnalysis, GrazingActivity? grazingActivity)
 		{
 			Dictionary<FeedProduct, float> rationProducts = new();
 
-			IReadOnlyList<Roughage> roughages = feedProducts.OfType<Roughage>().ToList();
-			IReadOnlyList<SupplementaryFeedProduct> supplementaryFeedProducts = feedProducts.OfType<SupplementaryFeedProduct>().ToList();
+			// TODO test wat er wordt gedaan met ruwvoer producten die wel of niet een VEM en/of RE waarde bevatten
+			// TODO test de conditie wanneer een ruwvoerproduct niet beschikbaar is maar wel aan de functie wordt gegeven.
+			List<FeedProduct> availableFeedProducts =
+				feedProducts.Where(r => r is { Available: true, FeedAnalysis: { VEM: { }, RE: { } } }).ToList();
+
+			IReadOnlyList<Roughage> roughages = availableFeedProducts.OfType<Roughage>().ToList();
+			IReadOnlyList<SupplementaryFeedProduct> supplementaryFeedProducts = availableFeedProducts.OfType<SupplementaryFeedProduct>().ToList();
 
 			if (herd.NumberOfAnimals == 0)
 			{
@@ -38,12 +48,7 @@ namespace GripOpGras2.Client.Features.CreateRation
 				});
 			}
 
-			// TODO test wat er wordt gedaan met ruwvoer producten die wel of niet een VEM en/of RE waarde bevatten
-			// TODO test de conditie wanneer een ruwvoerproduct niet beschikbaar is maar wel aan de functie wordt gegeven.
-			List<Roughage> availableRoughages =
-				roughages.Where(r => r is { Available: true, FeedAnalysis: { VEM: { }, RE: { } } }).ToList();
-
-			if (availableRoughages.Count == 0)
+			if (availableFeedProducts.Count == 0)
 			{
 				return await Task.FromResult(new FeedRation
 				{
@@ -56,14 +61,59 @@ namespace GripOpGras2.Client.Features.CreateRation
 			}
 
 			float totalDryMatterIntakeInKg = totalGrassIntake;
+
+			float dryMatterIntakeCow = totalDryMatterIntakeInKg / herd.NumberOfAnimals;
+			// Positive value is too much RE and a negative value is too little RE.
+			float deviationFromDesiredReCoverageForEachVemInTheGrass = (float)((OptimalRECoverageInGramsPerKgDm - grazingActivity.Plot.FeedAnalysis.RE) / grazingActivity.Plot.FeedAnalysis.VEM * -1);
+			float amountOfKgGrassForEachVem = (float)(1 / grazingActivity.Plot.FeedAnalysis.VEM);
+			float amountOfProteinInTheGrassToBeCompensated = (float)(grazingActivity.Plot.FeedAnalysis.VEM *
+																	 deviationFromDesiredReCoverageForEachVemInTheGrass *
+																	 totalGrassIntake);
+
+
+			Dictionary<FeedProduct, float> deviationFromDesiredReCoverageForEachProduct = new();
+
+			foreach (SupplementaryFeedProduct supplementaryFeedProduct in supplementaryFeedProducts)
+			{
+				float deviationFromDesiredRe =
+					(float)((OptimalRECoverageInGramsPerKgDm - supplementaryFeedProduct.FeedAnalysis.RE) / supplementaryFeedProduct.FeedAnalysis.VEM * -1);
+				deviationFromDesiredReCoverageForEachProduct.Add(supplementaryFeedProduct, deviationFromDesiredRe);
+			}
+
+
+			//pick the Roughage with the biggest negative RE of grass deviation
+
+			Roughage roughageWithBiggestReDeviation;
+
+			if (deviationFromDesiredReCoverageForEachVemInTheGrass > 0)
+			{
+				roughageWithBiggestReDeviation = roughages.OrderBy(r => deviationFromDesiredReCoverageForEachProduct[r]).First();
+			}
+			else if (deviationFromDesiredReCoverageForEachVemInTheGrass < 0)
+			{
+				roughageWithBiggestReDeviation = roughages.OrderBy(r => deviationFromDesiredReCoverageForEachProduct[r]).Last();
+			}
+			else
+			{
+				throw new NotImplementedException("add support for grass with RE of 0");
+			}
+
 			float vemIntake = 0;
-			float proteinIntakeInKg = 0;
+			float proteinIntakeInG = 0;
 
 			if (grazingActivity != null)
 			{
 				vemIntake = (float)(grazingActivity.Plot.FeedAnalysis.VEM * totalGrassIntake);
-				proteinIntakeInKg = (float)(grazingActivity.Plot.FeedAnalysis.RE * totalGrassIntake / 1000);
+				proteinIntakeInG = (float)(grazingActivity.Plot.FeedAnalysis.RE * totalGrassIntake);
 			}
+
+			//pick the amount VEM of roughageWithBiggestNegativeReDeviation to compensate the REdeviation of grass
+			float aanvullenVEMMetHoogstTegenpooLvanEiwittekortCverschot =
+
+
+			//////////--------------------------//////////
+			//////////--------------------------//////////
+			//////////--------------------------//////////
 
 			float vemNeeds = CalculateVemNeedsOfTheHerd(herd, milkProductionAnalysis);
 
@@ -149,7 +199,7 @@ namespace GripOpGras2.Client.Features.CreateRation
 		public float CalculateVemNeedsOfTheHerd(Herd herd, MilkProductionAnalysis milkProductionAnalysis)
 		{
 			float milkProductionForEachCow = milkProductionAnalysis.Amount / herd.NumberOfAnimals;
-			float vemNeedsCow = (milkProductionForEachCow * VemNeedsPerLiterMilk + DefaultVEMNeeds) *
+			float vemNeedsCow = (milkProductionForEachCow * VemNeedsPerLiterMilk + DefaultVEMNeedsOfCow) *
 								OptimalVEMCoverage;
 
 			return herd.NumberOfAnimals * vemNeedsCow;
