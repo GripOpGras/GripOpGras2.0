ebdf81333813347fcbb3704d8b3cf993b28fac0c
Chris <54854679+cvlnomen@users.noreply.github.com>
2022-12-15 15:19:14 +0100

    Proberen login probleem op te lossen

--- GripOpGras2.Client/Program.cs
+++ GripOpGras2.Client/Program.cs
@@ -1,6 +1,4 @@
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace GripOpGras2.Client
@@ -17,44 +15,20 @@ namespace GripOpGras2.Client
 			//{ BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 
-
-			//builder.Services.AddCors(options =>
-			//{
-			//	options.AddDefaultPolicy(
-			//		builder =>
-			//		{
-
-			//			//you can configure your custom policy
-			//			builder.AllowAnyOrigin()
-			//				.AllowAnyHeader()
-			//				.AllowAnyMethod();
-			//		});
-			//});
-
-			////.............
-
-			//app.UseCors();
-
-			builder.Services.AddScoped<CustomAuthenticationMessageHandler>();
-			builder.Services.AddHttpClient("api", client => client.BaseAddress = new Uri("https://accounts.test.farmmaps.eu/"))
-				.AddHttpMessageHandler<CustomAuthenticationMessageHandler>();
-			builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("api"));
+			//builder.Services.AddScoped<CustomAuthenticationMessageHandler>();
+			//builder.Services.AddHttpClient("api", client => client.BaseAddress = new Uri("https://accounts.test.farmmaps.eu/"))
+			//	.AddHttpMessageHandler<CustomAuthenticationMessageHandler>();
+			//builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("api"));
 
 			builder.Services.AddOidcAuthentication(options =>
 			{
-				options.ProviderOptions.Authority = "https://accounts.test.farmmaps.eu/";
-				options.ProviderOptions.ClientId = "farmmapsdev";
-				options.ProviderOptions.ResponseType = "code";
+				builder.Configuration.Bind("FarmMaps", options.ProviderOptions);
+
 				options.ProviderOptions.DefaultScopes.Add("api");
 				options.ProviderOptions.DefaultScopes.Add("profile");
-				options.ProviderOptions.DefaultScopes.Add("email");
-				options.ProviderOptions.DefaultScopes.Add("offline_access");
-				options.ProviderOptions.DefaultScopes.Add("openid");
-
-				//builder.Configuration.Bind("FarmMaps", options.ProviderOptions);
-				//options.ProviderOptions.ResponseType = "code";
-				////TODO mogelijk scopes toe voegen over de gebruiker profile
-				//options.ProviderOptions.DefaultScopes.Add("api");
+				//options.ProviderOptions.DefaultScopes.Add("email");
+				//options.ProviderOptions.DefaultScopes.Add("offline_access");
+				//options.ProviderOptions.DefaultScopes.Add("openid");
 			});
 
 			//builder.Services.AddAuthorizationCore(IISD)
@@ -63,12 +37,12 @@ namespace GripOpGras2.Client
 		}
 	}
 
-	public class CustomAuthenticationMessageHandler : AuthorizationMessageHandler
-	{
-		public CustomAuthenticationMessageHandler(IAccessTokenProvider provider, NavigationManager navigationManager)
-			: base(provider, navigationManager)
-		{
-			ConfigureHandler(new[] { "https://accounts.farmmaps.eu/" });
-		}
-	}
+	//public class CustomAuthenticationMessageHandler : AuthorizationMessageHandler
+	//{
+	//	public CustomAuthenticationMessageHandler(IAccessTokenProvider provider, NavigationManager navigationManager)
+	//		: base(provider, navigationManager)
+	//	{
+	//		ConfigureHandler(new[] { "https://accounts.farmmaps.eu/" });
+	//	}
+	//}
 }
