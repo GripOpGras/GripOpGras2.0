75585641823dd10a22856e08c7efe5200ae46902
Chris <54854679+cvlnomen@users.noreply.github.com>
2022-12-19 14:10:00 +0100

    Added custom exceptions

---
+++ GripOpGras2.Client/Data/Exceptions/RationAlgorithmExceptions/RationAlgorithmException.cs
@@ -0,0 +1,14 @@
+﻿namespace GripOpGras2.Client.Data.Exceptions.RationAlgorithmExceptions
+{
+	public class RationAlgorithmException : Exception
+	{
+		public RationAlgorithmException() : base("An error occurred during the preparation of the feed ration.")
+		{
+		}
+
+		public RationAlgorithmException(string message) : base(
+			$"An error occurred during the preparation of the feed ration: {message}.")
+		{
+		}
+	}
+}
