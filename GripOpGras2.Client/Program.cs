59324d62d76fb3a8ed48a0b3b1aa0e2cae5aa5dc
Chris <54854679+cvlnomen@users.noreply.github.com>
2022-12-20 21:15:37 +0100

    Make authorizes api calls to FarmMaps possible

--- GripOpGras2.Client/Program.cs
+++ GripOpGras2.Client/Program.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 namespace GripOpGras2.Client
@@ -11,18 +13,37 @@ namespace GripOpGras2.Client
 			builder.RootComponents.Add<App>("#app");
 			builder.RootComponents.Add<HeadOutlet>("head::after");
 
-			builder.Services.AddScoped(sp => new HttpClient
-			{ BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+			builder.Services.AddScoped<CustomAuthorizationMessageHandler>();
+
+			builder.Services.AddHttpClient("FarmMapsApi", client =>
+				{
+					client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("FarmMapsBaseUrl"));
+				})
+				.AddHttpMessageHandler<CustomAuthorizationMessageHandler>();
+
+			builder.Services.AddScoped(
+				sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("FarmMapsApi"));
 
 			builder.Services.AddOidcAuthentication(options =>
 			{
-				builder.Configuration.Bind("FarmMaps", options.ProviderOptions);
-				options.ProviderOptions.DefaultScopes.Add("openid");
-				options.ProviderOptions.DefaultScopes.Add("profile");
-				options.ProviderOptions.DefaultScopes.Add("api");
+				// See the following link for the supported scopes: https://accounts.test.farmmaps.eu/.well-known/openid-configuration
+				builder.Configuration.Bind("FarmMapsOidc", options.ProviderOptions);
 			});
 
 			await builder.Build().RunAsync();
+		}
+	}
+
+	public class CustomAuthorizationMessageHandler : AuthorizationMessageHandler
+	{
+		public CustomAuthorizationMessageHandler(IAccessTokenProvider provider,
+			NavigationManager navigationManager)
+			: base(provider, navigationManager)
+		{
+			ConfigureHandler(
+				authorizedUrls: new[] { "https://accounts.test.farmmaps.eu", "https://test.farmmaps.eu" }
+			);
+
 		}
 	}
 }
