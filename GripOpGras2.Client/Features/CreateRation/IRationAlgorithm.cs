850b75c75bbb408ab57c6c6fb0137b73061af3f5
Chris <54854679+cvlnomen@users.noreply.github.com>
2022-12-21 16:34:15 +0100

    update models and interfaces

--- GripOpGras2.Client/Features/CreateRation/IRationAlgorithm.cs
+++ GripOpGras2.Client/Features/CreateRation/IRationAlgorithm.cs
@@ -1,10 +1,15 @@
 ﻿using GripOpGras2.Domain;
+using GripOpGras2.Domain.FeedProducts;
 
 namespace GripOpGras2.Client.Features.CreateRation
 {
 	public interface IRationAlgorithm
 	{
-		public Task<FeedRation> CreateRationAsync(IReadOnlyList<Roughage> roughages, Herd herd, float totalGrassIntake,
-			GrazingActivity grazingActivity, MilkProductionAnalysis milkProductionAnalysis);
+		/// <summary>
+		/// Calculates the ration for the herd.
+		/// </summary>
+		/// <param name="totalGrassIntake">The total grass intake of the herd in kg dm.</param>
+		public Task<FeedRation> CreateRationAsync(IReadOnlyList<FeedProduct> feedProducts, Herd herd, float totalGrassIntake,
+			MilkProductionAnalysis milkProductionAnalysis, GrazingActivity? grazingActivity);
 	}
 }
